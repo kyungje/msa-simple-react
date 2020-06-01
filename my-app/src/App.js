941b3d1d9e18ed0ffad9bd4ff45baa3fcf1c48e8
kyungje@gmail.com <kyungje@gmail.com>
2020-06-01 17:44:10 +0900

    commit test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'홍길동',
+  'birthday':'820101',
+  'gender':'남',
+  'job':'프로'
+}
 
 function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop system!</h2>
-    </div>
+  return ( 
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
